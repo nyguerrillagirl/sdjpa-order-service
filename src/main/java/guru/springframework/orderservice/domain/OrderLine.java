d0d39e45a7eebf612bf08ae3500b7de42e902650
nyguerrillagirl <lorraine.figueroa@brainycode.com>
2023-10-28 13:17:37 -0400

    Completed 134. Many to One Unidirectional

--- src/main/java/guru/springframework/orderservice/domain/OrderLine.java
+++ src/main/java/guru/springframework/orderservice/domain/OrderLine.java
@@ -13,6 +13,9 @@ public class OrderLine extends BaseEntity {
     @ManyToOne
     private OrderHeader orderHeader;
 
+    @ManyToOne
+    private Product product;
+
     public Integer getQuantityOrdered() {
         return quantityOrdered;
     }
@@ -29,6 +32,14 @@ public class OrderLine extends BaseEntity {
         this.orderHeader = orderHeader;
     }
 
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -39,13 +50,16 @@ public class OrderLine extends BaseEntity {
 
         if (!Objects.equals(quantityOrdered, orderLine.quantityOrdered))
             return false;
-        return Objects.equals(orderHeader, orderLine.orderHeader);
+        if (!Objects.equals(orderHeader, orderLine.orderHeader))
+            return false;
+        return Objects.equals(product, orderLine.product);
     }
 
     @Override
     public int hashCode() {
         int result = super.hashCode();
         result = 31 * result + (quantityOrdered != null ? quantityOrdered.hashCode() : 0);
+        result = 31 * result + (product != null ? product.hashCode() : 0);
         return result;
     }
 }
