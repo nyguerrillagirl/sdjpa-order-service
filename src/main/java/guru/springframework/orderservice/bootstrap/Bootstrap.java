1c5f8d90c28585af245def1ecd23a37a729e50c6
nyguerrillagirl <lorraine.figueroa@brainycode.com>
2023-11-25 18:46:53 -0500

    Completed Lecture 153. Lazy Initialize Error

--- src/main/java/guru/springframework/orderservice/bootstrap/Bootstrap.java
+++ src/main/java/guru/springframework/orderservice/bootstrap/Bootstrap.java
@@ -1,12 +1,31 @@
 package guru.springframework.orderservice.bootstrap;
 
+import guru.springframework.orderservice.domain.Category;
+import guru.springframework.orderservice.domain.OrderHeader;
+import guru.springframework.orderservice.repositories.OrderHeaderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 @Component
 public class Bootstrap implements CommandLineRunner {
+
+    @Autowired
+    OrderHeaderRepository orderHeaderRepository;
+
+    @Transactional
     @Override
     public void run(String... args) throws Exception {
-        System.out.println("I was called!");
+        OrderHeader orderHeader = orderHeaderRepository.findById(1L).get();
+
+        orderHeader.getOrderLines().forEach(ol -> {
+            System.out.println(ol.getProduct().getDescription());
+
+            ol.getProduct().getCategories().forEach(cat -> {
+                System.out.println("HERE!");
+                System.out.println(cat.getDescription());
+            });
+        });
     }
 }
