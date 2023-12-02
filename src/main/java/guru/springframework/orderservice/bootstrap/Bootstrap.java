395c11c18449bb5dd2848d1c152f4e27cd0c56a4
nyguerrillagirl <lorraine.figueroa@brainycode.com>
2023-12-02 12:42:49 -0500

    Completed Lecture 154. Transaction Proxy Mode.

--- src/main/java/guru/springframework/orderservice/bootstrap/Bootstrap.java
+++ src/main/java/guru/springframework/orderservice/bootstrap/Bootstrap.java
@@ -1,31 +1,17 @@
 package guru.springframework.orderservice.bootstrap;
 
-import guru.springframework.orderservice.domain.Category;
-import guru.springframework.orderservice.domain.OrderHeader;
-import guru.springframework.orderservice.repositories.OrderHeaderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Transactional;
 
 @Component
 public class Bootstrap implements CommandLineRunner {
 
     @Autowired
-    OrderHeaderRepository orderHeaderRepository;
+    BootstrapOrderService bootstrapOrderService;
 
-    @Transactional
     @Override
     public void run(String... args) throws Exception {
-        OrderHeader orderHeader = orderHeaderRepository.findById(1L).get();
-
-        orderHeader.getOrderLines().forEach(ol -> {
-            System.out.println(ol.getProduct().getDescription());
-
-            ol.getProduct().getCategories().forEach(cat -> {
-                System.out.println("HERE!");
-                System.out.println(cat.getDescription());
-            });
-        });
-    }
+        bootstrapOrderService.readOrderData();
+     }
 }
